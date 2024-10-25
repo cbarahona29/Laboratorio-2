68ef6e1ccd2d7900b4a127de67b6620dbb288906
cbarahona29 <onnhyx1g@gmail.com>
2024-10-25 11:42:50 -0600

    Ejercicios de Notas, Cadenas

---
+++ src/lab2/Garcia_Cesar_Cadena.java
@@ -0,0 +1,46 @@
+
+package lab2;
+
+import java.util.Scanner;
+
+public class Garcia_Cesar_Cadena {
+    public static void main(String [] argp){
+     Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Ingresar cantidad de notas:");
+        int cnotas = entrada.nextInt();
+        int contador = 1;
+        int sumaNotas = 0;
+        int notamax = Integer.MIN_VALUE; 
+        int notamin = Integer.MAX_VALUE; 
+        
+        while (contador <= cnotas) {
+            int nota = -1;
+            
+            while (nota < 0 || nota > 100) {
+                System.out.println("Ingresar nota #" + contador);
+                nota = entrada.nextInt();
+                
+                if (nota < 0 || nota > 100) {
+                    System.out.println("Ingrese valores entre 0 y 100");
+                }
+            }
+            
+            sumaNotas += nota; 
+               if (nota > notamax) {
+                notamax = nota;
+            }
+            if (nota < notamin) {
+                notamin = nota;
+            }
+            contador++;
+        }
+
+       
+        double promedio = sumaNotas / cnotas;
+        System.out.println("El promedio de las notas es: " + promedio);
+        System.out.println("La nota mas alta es: " + notamax);
+        System.out.println("La nota mas baja es: " + notamin);
+        
+        }
+}
